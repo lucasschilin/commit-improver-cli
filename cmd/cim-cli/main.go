3d5a0f6c8eeb3203f8ed5f4676a24a54519669a9
lucasschilin <schilin.lucas@gmail.com>
2026-03-17 18:52:06 -0300

    feat: implement ox.Exit(1) at main.go

--- cmd/cim-cli/main.go
+++ cmd/cim-cli/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/lucasschilin/cim-cli/cmd"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		fmt.Println(err)
+		os.Exit(1)
 	}
 }
