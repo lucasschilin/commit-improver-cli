faf943366c5e631e1a865155b174d18d99b668fa
lucasschilin <schilin.lucas@gmail.com>
2026-03-29 15:39:39 -0300

    fix(config): check for prompt config not found only if no error

--- cmd/config_prompt_show.go
+++ cmd/config_prompt_show.go
@@ -38,8 +38,7 @@ var configPromptShowCmd = &cobra.Command{
 
 		if err != nil {
 			return fmt.Errorf("Prompt config error: %v", err)
-		}
-		if !found && (globalFlag || repoUserFlag || repoFlag) {
+		} else if !found && (globalFlag || repoUserFlag || repoFlag) {
 			return errors.New("Prompt config not found")
 		}
 
